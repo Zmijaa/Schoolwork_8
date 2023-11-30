5af25a96b6c38c513ff3127b69a0cf442860e657
Filip Mircev <147530688+Zmijaa@users.noreply.github.com>
2023-11-30 09:13:07 +0100

    Add files via upload

---
+++ src/myVar.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class myVar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner myVar=new
+Scanner (System.in);
+System.out.println(myVar.nextLine());
+
+	}
+
+}
